180d516880d3e614e8864ac84fa95ea6701005ab
zyfncg <1370305206@qq.com>
2018-04-26 21:58:08 +0800

    添加注释

--- digits/tools/k8s/k8s_coordinate.py
+++ digits/tools/k8s/k8s_coordinate.py
@@ -33,11 +33,21 @@ class KubernetasCoord(object):
 
     @staticmethod
     def generate_yaml(job_dir, pod_label, node_count, gpu_count):
+        """
+        根据用户参数生成用于创建容器组的yaml配置文件
+        :param job_dir: job路径
+        :param pod_label:
+        :param node_count: 节点数量
+        :param gpu_count: 每个节点GPU数量
+        :return: 生成的yaml文件的路径
+        """
 
         digits_path = os.path.dirname(digits.__file__)
         yaml_path = os.path.join(digits_path, 'tools/k8s/mpi_node_base.yaml')
         new_yaml_path = os.path.join(job_dir, 'mpi-nodes.yaml')
         name = pod_label
+
+        # 读取mpi_node_base.yaml中的内容，并将可修改的参数进行替换，保存成新的yaml文件
         with open(yaml_path, mode='r+') as r_file:
             with open(new_yaml_path, 'w+') as w_file:
                 for line in r_file.readlines():
@@ -58,25 +68,45 @@ class KubernetasCoord(object):
         return new_yaml_path
 
     def create_deployment(self, yaml_path, pod_label):
+        """
+        创建deployment类型的容器组
+        :param yaml_path: yaml文件路径
+        :param pod_label: 容器组label
+        :return: 无
+        """
+
+        # 读取yaml文件
         with open(yaml_path) as f:
             dep = yaml.load(f)
             resp = client.ExtensionsV1beta1Api().create_namespaced_deployment(body=dep, namespace="default")
             self.logger.info("Deployment created. status='%s'" % str(resp.status))
         running = False
+
+        # 由于容器组启动需要一定时间，使用轮询判断deployment是否成功启动
+        # 当所有容器都启动成功时，running设置为True，退出循环，结束轮询
         while not running:
+            # 轮询时间间隔为1秒
             time.sleep(1)
+            # 获取deployment创建状态
             status = self.get_pods_status(pod_label=pod_label)
             if len(status) > 0:
                 self.logger.info(status)
                 running = True
                 for stat in status:
+                    # 如果有任何一个容器状态不为Running,表示未全部成功启动
                     if not stat == 'Running':
+                        # 未全部成功启动，需要继续轮询
                         running = False
                         break
         self.logger.info("Deployment created successfully!")
 
     @staticmethod
     def delete_deployment(name):
+        """
+        删除deployment
+        :param name: 要删除的deployment的name
+        :return: 无
+        """
         # Delete deployment
         api_response = client.ExtensionsV1beta1Api().delete_namespaced_deployment(
             name=name,
